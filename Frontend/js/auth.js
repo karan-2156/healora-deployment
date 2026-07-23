bebea584aec24d93e8430d439d2377064bd555a5
Karan Singh <karankaransingh2156@gmail.com>
2026-07-23 11:19:19 +0530

    Fix production API URL

--- Frontend/js/auth.js
+++ Frontend/js/auth.js
@@ -2,7 +2,7 @@
    HEALORA AUTHENTICATION
    auth.js
 ========================================== */
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://healora-deployment.onrender.com/api";
 
 const token = localStorage.getItem("token");
 
